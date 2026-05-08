e0b35be35f8554515f68d894463caed1e332ae5f
RoshanDahal123 <dahalroshan765@gmail.com>
2026-05-08 23:18:15 +0545

    chore: update deployment configuration

--- src/https/index.ts
+++ src/https/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_API_URL|| "http://localhost:4000/api/";
+// const baseURL = import.meta.env.VITE_API_URL|| "http://localhost:4000/api/";
+const baseURL = import.meta.env.VITE_API_URL|| "https://mern-digitaldhokanproject.onrender.com/api/";
 export const API = axios.create({
   baseURL: baseURL,
   headers: {
